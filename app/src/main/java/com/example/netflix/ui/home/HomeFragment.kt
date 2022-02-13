98bf2597aba1247e0b37c2b99c50ea9327aa4088
menasri23 <m.e26159487@gmail.com>
2022-02-13 03:27:13 -0800

    Get access to view by using data binding in home_fragment.xml.

--- app/src/main/java/com/example/netflix/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/netflix/ui/home/HomeFragment.kt
@@ -1,23 +1,28 @@
 package com.example.netflix.ui.home
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import com.example.netflix.R
+import com.example.netflix.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
     private val viewModel by viewModels<HomeViewModel>()
+    private lateinit var binding: HomeFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
+    ): View {
+        binding = DataBindingUtil
+            .inflate(layoutInflater, R.layout.home_fragment, container, false)
+
+        return binding.root
     }
 
 }
