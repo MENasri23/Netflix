f0835afde032c17fd9b7d28ebb57986948a8eb93
menasri23 <m.e26159487@gmail.com>
2022-02-13 03:22:16 -0800

    Add new fragment HomeFragment.kt with associated stateHolder.

---
+++ app/src/main/java/com/example/netflix/ui/home/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.netflix.ui.home
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.netflix.R
+
+class HomeFragment : Fragment() {
+
+    private val viewModel by viewModels<HomeViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+
+}
