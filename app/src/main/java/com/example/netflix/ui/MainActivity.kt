b18c780d96c60db1cf18844d4703be28e6c34b54
menasri23 <m.e26159487@gmail.com>
2022-02-13 02:26:34 -0800

    move MainActivity.kt to package 'ui'.

--- app/src/main/java/com/example/netflix/ui/MainActivity.kt
+++ app/src/main/java/com/example/netflix/ui/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.example.netflix
+package com.example.netflix.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.netflix.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
