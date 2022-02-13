324a67ca572a8b3849ce1f520b3d4b75b4287866
menasri23 <m.e26159487@gmail.com>
2022-02-13 03:05:49 -0800

    Get reference to navHostFragment in activity and find navController for navigating.

--- app/src/main/java/com/example/netflix/ui/MainActivity.kt
+++ app/src/main/java/com/example/netflix/ui/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.netflix.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.netflix.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
 
     }
 }
